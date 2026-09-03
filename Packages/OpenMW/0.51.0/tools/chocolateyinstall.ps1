db985d4e20d6bfac1f62b9784f2d2e08d75bcc6f
AnyOldName3 <krizdjali@gmail.com>
2026-09-03 17:00:43 +0100

    Add 0.51.0 package
    I thought I did this already. Oops

---
+++ Packages/OpenMW/0.51.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'openmw'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'exe'
+  url64bit       = 'https://github.com/OpenMW/openmw/releases/download/openmw-0.51.0/OpenMW-0.51.0-Windows-x64.exe'
+
+  checksum64     = '56f6ccda630860a3197fb10c802e047740ea92230474dc442a39674e227f9ce7'
+  checksumtype64 = 'sha256'
+
+  silentArgs     = "/S"
+  validExitCodes = @(0)
+
+  registryUninstallerKey = 'OpenMW 0.51.0'
+}
+
+Install-ChocolateyPackage @packageArgs
